9e646360810554a83e0342c6abcbcedb47d6b2d1
Wuryannnnn <rwu03475@usc.edu>
2023-07-17 13:59:46 -0700

    feat: redis operate

---
+++ src/cache/_redis.js
@@ -0,0 +1,66 @@
+/**
+ * @description 连接 redis 的方法 get set
+ * @author wurui
+ */
+
+const redis = require('redis');
+const { REDIS_CONF } = require('../conf/db');
+
+// 创建客户端
+(async () => {
+    const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
+    await redisClient.connect().then(() => {
+        console.log('redis connect success')
+    }
+    ).catch(err => {
+        console.log('redis connect error', err)
+    }
+    )
+
+    redisClient.quit()
+})()
+
+/**
+ * redis set
+ * @param {string} key key
+ * @param {string} val val
+ * @param {number} timeout 过期时间，单位 s
+ */
+
+async function set(key, val, timeout = 60 * 60) {
+    if(typeof val === 'object') {
+        val = JSON.stringify(val)
+    }
+    await redisClient.set(key, val)
+    redisClient.expire(key, timeout)
+}
+
+/**
+ * redis get
+ * @param {string} key key
+ * @return {Promise}
+ * 
+ */
+
+async function get(key) {
+    try {
+        const val = await redisClient.get(key)
+        if(val == null) {
+            return null
+        }
+        try {
+            return JSON.parse(val)
+        } catch (ex) {
+            return val
+        }
+    } catch (ex) {
+        console.error(ex)
+        return null
+    }
+}
+
+
+module.exports = {
+    set,
+    get
+}
